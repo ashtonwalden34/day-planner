7393ce9ac111a58c993550549a983286e6d0e634
Ashton Walden <ashtonwalden@icloud.com>
2020-05-05 23:16:42 -0600

    Added current date and time blocks

---
+++ script.js
@@ -0,0 +1,10 @@
+// function to wrap everything, run when page is loaded
+$(document).ready(function() {
+    // gets the current date from moment.js, adds it as text into "currentDay", formats it show month, day, and year
+    $("#currentDay").text(moment().format("MMMM Do YYYY"));
+    
+
+  
+  
+    
+});
